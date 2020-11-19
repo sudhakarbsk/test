29369188ef8a7e7c6447055ba59fdc826ef6e1a9
sudhakarbsk <68243238+sudhakarbsk@users.noreply.github.com>
2020-11-19 14:29:38 +0530

    Add files via upload

---
+++ test/java/com/example/FooTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.Test;
+
+public class FooTest {
+    @Test
+    public void bar_prints() {
+        Foo testObject = new Foo();
+        testObject.bar();
+    }
+}
